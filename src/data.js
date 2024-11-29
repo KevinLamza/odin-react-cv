4f496e24c1fc242b8a01ad9e0f78e4b8932f0893
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2024-11-29 23:44:36 +0100

    Add data.js to be used as initial input

---
+++ src/data.js
@@ -0,0 +1,28 @@
+export const formGeneralInformation = {
+  name: 'Kevin Lamza',
+  email: 'mail@mail.net',
+  phone: '+49 123 456 789',
+  address: 'Somewhere, Europe',
+};
+
+export const formEducation = {
+  0: {
+    school: 'Universität des Saarlandes',
+    degree: 'Bachelor of Engineering in Mechatronics',
+    startDate: 'tbd',
+    endDate: 'tbd',
+    location: 'Saarbrücken, GER',
+  },
+};
+
+export const formPracticalExperience = {
+  0: {
+    companyName: 'ZF Friedrichshafen AG',
+    positionTitle: 'Prüfmittelplaner',
+    startDate: 'tbd',
+    endDate: 'tbd',
+    location: 'Saarbrücken, GER',
+    description:
+      'Planen und Beschaffen von Prüfmitteln und Messtechnik für fertigungsnahe Serienüberwachung. Statistische Untersuchungen und Auswertungen.',
+  },
+};
