3bd19c4fc5ae9574c47aee7564beec744913a093
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2024-11-29 20:05:32 +0100

    Add basic static CV page with dummy content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import phone from './assets/phone.svg';
+import email from './assets/email.svg';
+import pin from './assets/pin.svg';
 
 // function App() {
 //   const [count, setCount] = useState(0);
@@ -35,5 +36,94 @@ import './App.css';
 // export default App;
 
 export default function App() {
-  return <p>Hi!</p>;
+  return (
+    <>
+      <div className="cv">
+        <GeneralInformation />
+        <Education />
+        <PracticalExperience />
+      </div>
+    </>
+  );
+}
+
+function GeneralInformation() {
+  return (
+    <>
+      <section className="generalInformation">
+        <h1>Kevin Lamza</h1>
+        <div className="spanContainer">
+          <span>
+            <img src={email} />
+            mail@mail.net
+          </span>
+          <span>
+            <img src={phone} />
+            +49 123 456 789
+          </span>{' '}
+          <span>
+            <img src={pin} />
+            Somewhere, Europe
+          </span>
+        </div>
+      </section>
+    </>
+  );
+}
+
+function Education() {
+  return (
+    <>
+      <section className="education">
+        <table>
+          <tr>
+            <th colSpan="2">
+              <h1>Education</h1>
+            </th>
+          </tr>
+          <tr>
+            <td className="time">
+              09/2014 - 08/2020 <br /> Saarbrücken, GER
+            </td>
+            <td>
+              <b>Universität des Saarlandes</b> <br /> Bachelor of Engineering
+              in Mechatronics
+            </td>
+          </tr>
+          <tr>
+            <td className="time">
+              09/2006 - 06/2014 <br /> Neunkirchen, GER
+            </td>
+            <td>
+              Gymnasium am Krebsberg <br /> Abitur: 1,8
+            </td>
+          </tr>
+        </table>
+      </section>
+    </>
+  );
+}
+
+function PracticalExperience() {
+  return (
+    <>
+      <section className="practicalExperience">
+        <table>
+          <tr>
+            <th colSpan="2">
+              <h1>Practical Experience</h1>
+            </th>
+          </tr>
+          <tr>
+            <td className="time">03/2021 - 03/2023</td>
+            <td>
+              <b>ZF Friedrichshafen AG</b> <br /> Prüfmittelplaner <br /> Planen
+              und Beschaffen von Prüfmitteln und Messtechnik für fertigungsnahe
+              Serienüberwachung. Statistische Untersuchungen und Auswertungen.
+            </td>
+          </tr>
+        </table>
+      </section>
+    </>
+  );
 }
