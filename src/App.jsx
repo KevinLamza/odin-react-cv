af5b20352ca942f77fa74b129d07110d0556c918
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2024-11-30 23:16:54 +0100

    Add shared state for the input fields and CV

--- src/App.jsx
+++ src/App.jsx
@@ -3,68 +3,193 @@ import './App.css';
 import phone from './assets/phone.svg';
 import email from './assets/email.svg';
 import pin from './assets/pin.svg';
-import education from './assets/education.svg';
+import educationImg from './assets/education.svg';
 import work from './assets/work.svg';
-import { formGeneralInformationgeneralInformation } from './data';
-import { formEducation } from './data';
-import { formPracticalExperience } from './data';
+import { defaultUserInfo } from './data';
+import { defaultEducation } from './data';
+import { defaultExperience } from './data';
+
+/* 
+defaultUserInfo -> defaultUserInfo
+defaultEducation -> defaultEducation
+defaultExperience -> defaultExperience
+
+userInfo -> userInfo
+education -> education
+experience -> experience
+
+currentEducation -> currentEducation
+currentExperience -> currentExperience
+
+handleChangeUserInfo -> handleChangeUserInfo
+
+EducationList -> EducationList
+ExperienceList -> ExperienceList
+
+InputUserInfo -> InputUserInfo
+InputExperience -> InputExperience
+*/
 
 export default function App() {
+  const [userInfo, setUserInfo] = useState(defaultUserInfo);
+
+  const [education, setEducation] = useState(defaultEducation);
+  const [experience, setExperience] = useState(defaultExperience);
+
+  const [currentEducation, setCurrentEducation] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+
+  function handleChangeUserInfo(value, field) {
+    setUserInfo({ ...userInfo, [field]: value });
+  }
+
+  function handleChangeEducation(value, field) {
+    setEducation({
+      ...education,
+      [currentEducation]: { ...education[currentEducation], [field]: value },
+    });
+  }
+
+  function handleChangeExperience(value, field) {
+    setExperience({
+      ...experience,
+      [currentExperience]: { ...experience[currentExperience], [field]: value },
+    });
+  }
+
   return (
     <>
       <div className="gridContainer">
         <div className="inputs">
-          <Input />
+          <Input
+            userInfo={userInfo}
+            education={education}
+            experience={experience}
+            currentEducation={currentEducation}
+            currentExperience={currentExperience}
+            handleChangeUserInfo={handleChangeUserInfo}
+            handleChangeEducation={handleChangeEducation}
+            handleChangeExperience={handleChangeExperience}
+          />
         </div>
         <div className="cv">
-          <GeneralInformation />
-          <Education />
-          <PracticalExperience />
+          <UserInfo userInfo={userInfo} />
+          <Education education={education} />
+          <Experience experience={experience} />
         </div>
       </div>
     </>
   );
 }
 
-function Input() {
+function EducationList({ education }) {
+  const titles = [];
+  for (let key in education) {
+    titles.push(
+      <button key={'button' + key} className="listButton">
+        {education[key]['school']}
+      </button>,
+    );
+  }
+  return <>{titles}</>;
+}
+
+function ExperienceList({ experience }) {
+  const titles = [];
+  for (let key in experience) {
+    titles.push(
+      <button key={'button' + key} className="listButton">
+        {experience[key]['companyName']}
+      </button>,
+    );
+  }
+  return <>{titles}</>;
+}
+
+function Input({
+  userInfo,
+  handleChangeUserInfo,
+  education,
+  experience,
+  currentEducation,
+  currentExperience,
+  handleChangeEducation,
+  handleChangeExperience,
+}) {
   return (
     <>
-      <InputGeneralInformation />
-      <InputEducation />
-      <InputPracticalExperience />
+      <InputUserInfo
+        userInfo={userInfo}
+        handleChangeUserInfo={handleChangeUserInfo}
+        education={education}
+      />
+      <InputEducation
+        education={education}
+        currentEducation={currentEducation}
+        handleChangeEducation={handleChangeEducation}
+      />
+      <InputExperience
+        experience={experience}
+        currentExperience={currentExperience}
+        handleChangeExperience={handleChangeExperience}
+      />
     </>
   );
 }
 
-function InputGeneralInformation() {
+function InputUserInfo({ userInfo, handleChangeUserInfo }) {
   return (
     <>
       <div className="form">
-        <button></button>
+        <button className="navButton"></button>
         <h2>Personal Details</h2>
         <form>
           <div className="formField">
             <label htmlFor="name">Full name: </label>
             <br />
-            <input type="text" name="name" required />
+            <input
+              type="text"
+              name="name"
+              value={userInfo.name}
+              onChange={(e) => handleChangeUserInfo(e.target.value, 'name')}
+              required
+            />
             <br />
           </div>
           <div className="formField">
             <label htmlFor="email">Email: </label>
             <br />
-            <input type="email" name="email" required />
+            <input
+              type="email"
+              name="email"
+              value={userInfo.email}
+              onChange={(e) => handleChangeUserInfo(e.target.value, 'email')}
+              required
+            />
             <br />
           </div>
           <div className="formField">
             <label htmlFor="tel">Phone number: </label>
             <br />
-            <input type="tel" name="tel" required />
+            <input
+              type="tel"
+              name="tel"
+              value={userInfo.phone}
+              onChange={(e) => handleChangeUserInfo(e.target.value, 'phone')}
+              required
+            />
             <br />
           </div>
           <div className="formField">
             <label htmlFor="address">Address: </label>
             <br />
-            <input type="text" name="address" required />
+            <input
+              type="text"
+              name="address"
+              value={userInfo.address}
+              onChange={(e) => handleChangeUserInfo(e.target.value, 'address')}
+              required
+            />
           </div>
           {/* <input type="submit" value="Subscribe!" /> */}
         </form>
@@ -73,121 +198,225 @@ function InputGeneralInformation() {
   );
 }
 
-function InputEducation() {
-  return (
-    <>
-      <div className="form">
-        <button></button>
-        <h2>
-          <img src={education} />
-          Education
-        </h2>
+function InputEducation({
+  education,
+  currentEducation,
+  handleChangeEducation,
+}) {
+  let display;
+  if (currentEducation === 'none') {
+    display = <EducationList education={education} />;
+  } else {
+    display = (
+      <>
         <form>
           <div className="formField">
             <label htmlFor="school">School: </label>
             <br />
-            <input type="text" name="school" required />
+            <input
+              type="text"
+              name="school"
+              value={education[currentEducation]['school']}
+              onChange={(e) => handleChangeEducation(e.target.value, 'school')}
+              required
+            />
             <br />
           </div>
           <div className="formField">
             <label htmlFor="degree">Degree: </label>
             <br />
-            <input type="text" name="degree" required />
+            <input
+              type="text"
+              name="degree"
+              value={education[currentEducation]['degree']}
+              onChange={(e) => handleChangeEducation(e.target.value, 'degree')}
+              required
+            />
             <br />
           </div>
           <div className="formField">
             <label htmlFor="startDate">Start Date: </label>
             <br />
-            <input type="date" name="startDate" required />
+            <input
+              type="text"
+              name="startDate"
+              value={education[currentEducation]['startDate']}
+              onChange={(e) =>
+                handleChangeEducation(e.target.value, 'startDate')
+              }
+              required
+            />
             <br />
           </div>
           <div className="formField">
             <label htmlFor="endDate">End Date: </label>
             <br />
-            <input type="date" name="endDate" required />
+            <input
+              type="text"
+              name="endDate"
+              value={education[currentEducation]['endDate']}
+              onChange={(e) => handleChangeEducation(e.target.value, 'endDate')}
+              required
+            />
             <br />
           </div>
           <div className="formField">
             <label htmlFor="location">Location: </label>
             <br />
-            <input type="text" name="location" required />
+            <input
+              type="text"
+              name="location"
+              value={education[currentEducation]['location']}
+              onChange={(e) =>
+                handleChangeEducation(e.target.value, 'location')
+              }
+              required
+            />
           </div>
           {/* <input type="submit" value="Subscribe!" /> */}
         </form>
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="form">
+        <button className={'navButton'}></button>
+        <h2>
+          <img src={educationImg} />
+          Education
+        </h2>
+        {display}
       </div>
     </>
   );
 }
 
-function InputPracticalExperience() {
+function InputExperience({
+  experience,
+  currentExperience,
+  handleChangeExperience,
+}) {
+  let display;
+  if (currentExperience === 'none') {
+    display = <ExperienceList experience={experience} />;
+  } else {
+    display = (
+      <form>
+        <div className="formField">
+          <label htmlFor="company">Company Name: </label>
+          <br />
+          <input
+            type="text"
+            name="company"
+            value={experience[currentExperience]['companyName']}
+            onChange={(e) =>
+              handleChangeExperience(e.target.value, 'companyName')
+            }
+            required
+          />
+          <br />
+        </div>
+        <div className="formField">
+          <label htmlFor="position">Position Title: </label>
+          <br />
+          <input
+            type="text"
+            name="position"
+            value={experience[currentExperience]['positionTitle']}
+            onChange={(e) =>
+              handleChangeExperience(e.target.value, 'positionTitle')
+            }
+            required
+          />
+          <br />
+        </div>
+        <div className="formField">
+          <label htmlFor="startDate">Start Date: </label>
+          <br />
+          <input
+            type="text"
+            name="startDate"
+            value={experience[currentExperience]['startDate']}
+            onChange={(e) =>
+              handleChangeExperience(e.target.value, 'startDate')
+            }
+            required
+          />
+          <br />
+        </div>
+        <div className="formField">
+          <label htmlFor="endDate">End Date: </label>
+          <br />
+          <input
+            type="text"
+            name="endDate"
+            value={experience[currentExperience]['endDate']}
+            onChange={(e) => handleChangeExperience(e.target.value, 'endDate')}
+            required
+          />
+          <br />
+        </div>
+        <div className="formField">
+          <label htmlFor="location">Location: </label>
+          <br />
+          <input
+            type="text"
+            name="location"
+            value={experience[currentExperience]['location']}
+            onChange={(e) => handleChangeExperience(e.target.value, 'location')}
+            required
+          />
+          <br />
+        </div>
+        <div className="formField">
+          <label htmlFor="description">Description: </label>
+          <br />
+          <input
+            type="text"
+            name="description"
+            value={experience[currentExperience]['description']}
+            onChange={(e) =>
+              handleChangeExperience(e.target.value, 'description')
+            }
+            required
+          />
+        </div>
+        {/* <input type="submit" value="Subscribe!" /> */}
+      </form>
+    );
+  }
   return (
     <>
       <div className="form">
-        <button></button>
+        <button className={'navButton'}></button>
         <h2>
           <img src={work} />
           Practical Experience
         </h2>
-        <form>
-          <div className="formField">
-            <label htmlFor="company">Company Name: </label>
-            <br />
-            <input type="text" name="company" required />
-            <br />
-          </div>
-          <div className="formField">
-            <label htmlFor="position">Position Title: </label>
-            <br />
-            <input type="text" name="degree" required />
-            <br />
-          </div>
-          <div className="formField">
-            <label htmlFor="startDate">Start Date: </label>
-            <br />
-            <input type="date" name="startDate" required />
-            <br />
-          </div>
-          <div className="formField">
-            <label htmlFor="endDate">End Date: </label>
-            <br />
-            <input type="date" name="endDate" required />
-            <br />
-          </div>
-          <div className="formField">
-            <label htmlFor="location">Location: </label>
-            <br />
-            <input type="text" name="location" required />
-            <br />
-          </div>
-          <div className="formField">
-            <label htmlFor="description">Description: </label>
-            <br />
-            <input type="text" name="description" required />
-          </div>
-          {/* <input type="submit" value="Subscribe!" /> */}
-        </form>
+        {display}
       </div>
     </>
   );
 }
 
-function GeneralInformation() {
+function UserInfo({ userInfo }) {
   return (
     <>
-      <div className="generalInformation">
-        <h1>Kevin Lamza</h1>
+      <div className="userInfo">
+        <h1>{userInfo.name}</h1>
         <div className="spanContainer">
           <span>
             <img src={email} />
-            mail@mail.net
+            {userInfo.email}
           </span>
           <span>
             <img src={phone} />
-            +49 123 456 789
+            {userInfo.phone}
           </span>{' '}
           <span>
             <img src={pin} />
-            Somewhere, Europe
+            {userInfo.address}
           </span>
         </div>
       </div>
@@ -195,57 +424,68 @@ function GeneralInformation() {
   );
 }
 
-function Education() {
+function Education({ education }) {
+  const list = [];
+  for (let key in education) {
+    list.push(
+      <tr key={'education' + key}>
+        <td className="time">
+          {education[key]['startDate']} - {education[key]['endDate']} <br />{' '}
+          {education[key]['location']}
+        </td>
+        <td>
+          <b>{education[key]['school']}</b> <br /> {education[key]['degree']}
+        </td>
+      </tr>,
+    );
+  }
   return (
     <>
       <div className="education">
         <table>
-          <tr>
-            <th colSpan="2">
-              <h1>Education</h1>
-            </th>
-          </tr>
-          <tr>
-            <td className="time">
-              09/2014 - 08/2020 <br /> Saarbrücken, GER
-            </td>
-            <td>
-              <b>Universität des Saarlandes</b> <br /> Bachelor of Engineering
-              in Mechatronics
-            </td>
-          </tr>
-          <tr>
-            <td className="time">
-              09/2006 - 06/2014 <br /> Neunkirchen, GER
-            </td>
-            <td>
-              <b>Gymnasium am Krebsberg</b> <br /> Abitur: 1,8
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <th colSpan="2">
+                <h1>Education</h1>
+              </th>
+            </tr>
+            {list}
+          </tbody>
         </table>
       </div>
     </>
   );
 }
 
-function PracticalExperience() {
+function Experience({ experience }) {
+  const list = [];
+  for (let key in experience) {
+    list.push(
+      <tr key={'experience' + key}>
+        <td className="time">
+          {experience[key]['startDate']} - {experience[key]['endDate']} <br />{' '}
+          {experience[key]['location']}
+        </td>
+        <td>
+          <b>{experience[key]['companyName']}</b> <br />{' '}
+          {experience[key]['positionTitle']} <br />{' '}
+          {experience[key]['description']}
+        </td>
+      </tr>,
+    );
+  }
   return (
     <>
-      <section className="practicalExperience">
+      <section className="experience">
         <table>
-          <tr>
-            <th colSpan="2">
-              <h1>Practical Experience</h1>
-            </th>
-          </tr>
-          <tr>
-            <td className="time">03/2021 - 03/2023</td>
-            <td>
-              <b>ZF Friedrichshafen AG</b> <br /> Prüfmittelplaner <br /> Planen
-              und Beschaffen von Prüfmitteln und Messtechnik für fertigungsnahe
-              Serienüberwachung. Statistische Untersuchungen und Auswertungen.
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <th colSpan="2">
+                <h1>Practical Experience</h1>
+              </th>
+            </tr>
+            {list}
+          </tbody>
         </table>
       </section>
     </>
--- src/data.js
+++ src/data.js
@@ -1,21 +1,28 @@
-export const formGeneralInformation = {
+export const defaultUserInfo = {
   name: 'Kevin Lamza',
   email: 'mail@mail.net',
   phone: '+49 123 456 789',
   address: 'Somewhere, Europe',
 };
 
-export const formEducation = {
+export const defaultEducation = {
   0: {
     school: 'Universität des Saarlandes',
     degree: 'Bachelor of Engineering in Mechatronics',
-    startDate: 'tbd',
-    endDate: 'tbd',
+    startDate: '09/2013',
+    endDate: '06/2019',
     location: 'Saarbrücken, GER',
+  },
+  1: {
+    school: 'Gymnasium am Krebsberg',
+    degree: 'Abitur 1.8',
+    startDate: '09/2005',
+    endDate: '06/2013',
+    location: 'Neunkirchen, GER',
   },
 };
 
-export const formPracticalExperience = {
+export const defaultExperience = {
   0: {
     companyName: 'ZF Friedrichshafen AG',
     positionTitle: 'Prüfmittelplaner',
@@ -24,5 +31,13 @@ export const formPracticalExperience = {
     location: 'Saarbrücken, GER',
     description:
       'Planen und Beschaffen von Prüfmitteln und Messtechnik für fertigungsnahe Serienüberwachung. Statistische Untersuchungen und Auswertungen.',
+  },
+  1: {
+    companyName: 'Black Mesa',
+    positionTitle: 'Scientist',
+    startDate: 'tbd',
+    endDate: 'tbd',
+    location: 'the Lab, USA',
+    description: 'Some fancy words about all the fancy stuff that I did',
   },
 };
